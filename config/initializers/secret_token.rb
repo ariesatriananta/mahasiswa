6a6135e4d33dea41be8d6e85c89fcac6d67b1c84
Arie Satriananta <ariesatriananta@yahoo.com>
2014-10-07 21:27:39 +0700

    inisialisasi repostori

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mahasiswa::Application.config.secret_token = '9bd5f510fe5d59f7c9458e47b84fa4ee890163993b7b721740d69e7a3b26fa9a62429c250b7caf3a32cfbc54755936b60a706a09ff795f942e0c72d5f2ef3bde'
